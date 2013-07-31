481eccf7c638b63691231d2053c4aa3070ddf81b
skynopi <skynopi@gmail.com>
2013-07-31 19:38:11 +0900

    KinsQ-RB social_comment module

---
+++ m_social/lang.korean/admin/_pc/var/var.menu.php
@@ -0,0 +1,5 @@
+<?php
+$d['amenu']['_info'] = '모듈정보';
+$d['amenu']['main'] = '소셜연동 설정';
+$d['amenu']['send'] = '내보낸 글';
+?>
---
+++ m_social/var/2.var.php
@@ -0,0 +1,18 @@
+<?php
+$d['social']['use_t'] = "1";
+$d['social']['key_t'] = "qCUhAbus6EdGmE4xGcWqzw";
+$d['social']['secret_t'] = "tQWzt1gk9fs23K3XiskhCcRAEElVadNNp0CPXyrXF1M";
+$d['social']['use_f'] = "1";
+$d['social']['key_f'] = "166769936802675";
+$d['social']['secret_f'] = "b0877359a0903f2a9e6d187ee51728ba";
+$d['social']['use_m'] = "1";
+$d['social']['key_m'] = "e5be6ec171fc8aa821630bb60146633a";
+$d['social']['secret_m'] = "";
+$d['social']['use_y'] = "1";
+$d['social']['key_y'] = "d2a619e3-e36e-4884-8ecb-3453f83dfd06";
+$d['social']['secret_y'] = "FHbwN2RClNJW1FuKMLjhHi4jnnwNeI9-MIywArms_B.iE2rZ78zD2Q00";
+$d['social']['use_b'] = "1";
+$d['social']['key_b'] = "julpuse";
+$d['social']['secret_b'] = "R_7069092d2195a076fb49899471ff69c1";
+$d['social']['key_id'] = "master";
+?>
